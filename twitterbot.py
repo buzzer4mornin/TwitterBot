0c3726862ae7eb226603509c88ba9155e5a809e8
Aydin Ahmadli <35424859+buzzer4mornin@users.noreply.github.com>
2020-10-08 00:47:09 +0200

    libraries

---
+++ twitterbot.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
+import numpy as np
+import random
+import time, os
+'''Uncomment the below line for Linux OS'''
+# from pyvirtualdisplay import Display
