afcb091cf3515a11b6792f70b58566354a887904
Aydin Ahmadli <35424859+buzzer4mornin@users.noreply.github.com>
2020-10-08 00:47:47 +0200

    setup class

--- twitterbot.py
+++ twitterbot.py
@@ -10,3 +10,16 @@ import time, os
 
 # TODO: within LOGIN, set 2 secs interval between username and password
 # TODO: here, instead of email, we will use username, and add email separately along with password [3 total arguments]
+
+class Twitterbot:
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+        # initializing chrome options
+        chrome_options = Options()
+        self.bot = webdriver.Chrome(
+            executable_path=os.path.join(os.getcwd(), 'chromedriver'),
+            options=chrome_options
+        )
