6e8c9c4da92ed534e45bc675421f3a83c3e812fc
Aydin Ahmadli <35424859+buzzer4mornin@users.noreply.github.com>
2020-10-08 00:48:51 +0200

    adding login function

--- twitterbot.py
+++ twitterbot.py
@@ -23,3 +23,30 @@ class Twitterbot:
             executable_path=os.path.join(os.getcwd(), 'chromedriver'),
             options=chrome_options
         )
+
+    def login(self):
+
+        bot = self.bot
+        bot.maximize_window()
+        bot.get('https://twitter.com/login')
+
+        time.sleep(3)
+        bot.implicitly_wait(10)
+
+        email = bot.find_element_by_css_selector(
+            '#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > div.css-1dbjc4n.r-13qz1uu > form > div > div:nth-child(6) > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1udh08x > div > input'
+        )
+        password = bot.find_element_by_css_selector(
+            '#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > div.css-1dbjc4n.r-13qz1uu > form > div > div:nth-child(7) > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1udh08x > div > input'
+        )
+
+        # sends the email to the email input
+        email.send_keys(self.email)
+        time.sleep(np.random.randint(1, 4))
+        # sends the password to the password input
+        password.send_keys(self.password)
+        time.sleep(np.random.randint(1, 4))
+        # executes RETURN key action
+        password.send_keys(Keys.RETURN)
+
+        bot.implicitly_wait(10)
