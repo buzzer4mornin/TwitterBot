e5cf89fe2c1f0f6e1cc541564f88eb4014193191
Aydin Ahmadli <35424859+buzzer4mornin@users.noreply.github.com>
2020-10-08 00:47:30 +0200

    #TODOs for later improvements

--- twitterbot.py
+++ twitterbot.py
@@ -7,3 +7,6 @@ import random
 import time, os
 '''Uncomment the below line for Linux OS'''
 # from pyvirtualdisplay import Display
+
+# TODO: within LOGIN, set 2 secs interval between username and password
+# TODO: here, instead of email, we will use username, and add email separately along with password [3 total arguments]
